26762d9a42b77a5f011367245f2bc419f082bf02
Jasper Van der Voo <jasper.voo@gmail.com>
2019-07-02 14:36:11 +1200

    Added progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,18 +1,31 @@
-import React from "react";
-import "./ProgressBar";
+import React, { useContext } from "react";
+import { ProgressContext } from "../contexts/ProgressContext";
+import "./ProgressBar.scss";
 export default function ProgressBar() {
+	const { progress, updateProgress } = useContext(ProgressContext);
+
+	// All possible application states. Should be in sync with the components in Content.js
+	const steps = [
+		"Nights & People",
+		"Special Diets",
+		"Meal Selection",
+		"Side Selection",
+		"Plan Selection",
+		"Checkout"
+	];
 	return (
-		<div className="progress-bar-outer">
-			<div className="progres-bar-inner">
-				<ol className="steps">
-					<li className="step">Nights & People</li>
-					<li className="step">Nights & People</li>
-					<li className="step">Nights & People</li>
-					<li className="step">Nights & People</li>
-					<li className="step">Nights & People</li>
-					<li className="step">Nights & People</li>
-				</ol>
-			</div>
+		<div className="progress-bar uppercase">
+			<ol className="steps">
+				{steps.map((item, index) => (
+					<li
+						key={index}
+						className={`step ${progress > index ? "complete" : ""} ${progress === index ? "current" : ""}`}
+						onClick={() => (index < progress ? updateProgress(index) : null)}
+					>
+						{item}
+					</li>
+				))}
+			</ol>
 		</div>
 	);
 }
