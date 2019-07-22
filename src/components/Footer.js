a9bdc6fec95adc41c62f759bd70470d6b5a386a1
Jasper Van der Voo <jasper.voo@gmail.com>
2019-07-22 16:34:06 +1200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,6 @@ import "./Footer.scss";
 export default function Footer() {
 	return (
 		<div className="footer">
-			<div className="footer-upper">
-				<i className="fab fa-facebook-f" />
-				<i className="fab fa-instagram" />
-			</div>
 			<div className="footer-lower">
 				<span className="uppercase">Copyright 2019 © DINNERin</span>
 			</div>
